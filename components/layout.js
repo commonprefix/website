2b4f505bb269c7ec1c994e9f84d95dbd91b6b2bd
k4m4 <nikolaskam@gmail.com>
2022-08-17 22:13:40 +0300

    Add space before `-` separator in page title

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ export default function Layout({ title, icon, children }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{`Common Prefix${title ? "- " + title : ""}`}</title>
+        <title>{`Common Prefix${title ? " - " + title : ""}`}</title>
         <link rel="icon" type="image/png" href={icon || "/philosopher-stone.png"} />
       </Head>
       <div className={styles.container}>{children}</div>
