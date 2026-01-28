60f7a60dd093ee2a3571d639dd7ebc56a719fbb6
torbognaes <torbognaes@gmail.com>
2026-01-28 17:32:37 +0100

    Remove DevOps position from Careers page

--- openings.go
+++ openings.go
@@ -16,13 +16,6 @@ var Openings = []JobOpening{
 		Description: "We're looking for experienced engineers who are passionate about blockchains, distributed systems and cryptography.",
 	},
 	{
-		Name:        "DevOps Engineer",
-		Url:         "https://commonprefix.notion.site/opening-devops-engineer",
-		Location:    "Remote or on-site in Athens, Greece",
-		Type:        "Full-time",
-		Description: "We're looking for DevOps engineers with strong systems instincts, a love for automation, and comfort operating distributed infrastructure.",
-	},
-	{
 		Name:        "Support Engineer",
 		Url:         "https://commonprefix.notion.site/Opening-Support-Engineer-2cabf672de8280438811f66b37f4b685?pvs=143",
 		Location:    "On-site in Athens, Greece",
